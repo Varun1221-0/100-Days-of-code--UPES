44ae61a4f34b6f5d33eae30a99d61aa9d4c837e2
Varun1221-0 <Varun.35403@stu.upes.ac.in>
2026-09-15 19:46:58 +0530

    Create d4q7.c

---
+++ d4q7.c
@@ -0,0 +1,19 @@
+// Q7: Write a program to swap two numbers without using a third variable.
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+
+    printf("Enter two numbers: ");
+    scanf("%d%d",&a,&b);
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    printf("After swapping: %d %d",a,b);
+
+    return 0;
+}
