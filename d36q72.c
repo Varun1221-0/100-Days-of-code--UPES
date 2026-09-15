02a1f76c979fba7c93393dc1c725a4c42c126aa9
Varun1221-0 <Varun.35403@stu.upes.ac.in>
2026-09-15 21:40:53 +0530

    Create d36q72.c

---
+++ d36q72.c
@@ -0,0 +1,26 @@
+// Q72: Find the sum of all elements in a matrix.
+
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10],r,c,i,j,sum=0;
+
+    printf("Enter rows and columns: ");
+    scanf("%d%d",&r,&c);
+
+    printf("Enter elements: ");
+
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+            sum=sum+a[i][j];
+        }
+    }
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
