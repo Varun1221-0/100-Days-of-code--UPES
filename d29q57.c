dac9b0e84f380bb006f70b5c7cd30030585aaa76
Varun1221-0 <Varun.35403@stu.upes.ac.in>
2026-09-15 21:28:00 +0530

    Implement array sum calculation in d29q57.c
    This program prompts the user for the size of an array and its elements, then calculates and prints the sum of those elements.

---
+++ d29q57.c
@@ -0,0 +1,22 @@
+// Q57: Find the sum of array elements.
+
+#include <stdio.h>
+
+int main()
+{
+    int a[100],n,i,sum=0;
+
+    printf("Enter size: ");
+    scanf("%d",&n);
+
+    printf("Enter elements: ");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+        sum=sum+a[i];
+    }
+
+    printf("Sum = %d",sum);
+
+    return 0;
+}
